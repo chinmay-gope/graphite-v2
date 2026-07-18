5f215a3bef9f783f4e35e160d1ec93ef63f05034
Chinmay <chinmaygope69@gmail.com>
2026-07-18 10:47:37 +0530

    feat: euler facade

---
+++ src/io/graphite/api/EulerService.java
@@ -0,0 +1,20 @@
+package io.graphite.api;
+
+import io.graphite.algorithm.euler.Hierholzer;
+import io.graphite.graph.IGraph;
+import io.graphite.result.EulerResult;
+
+public class EulerService extends GraphService {
+
+    EulerService(IGraph graph) {
+        super(graph);
+    }
+
+    public EulerResult findEulerPath() {
+        return new Hierholzer().findEulerPath(graph);
+    }
+
+    public EulerResult findEulerCircuit() {
+        return new Hierholzer().findEulerCircuit(graph);
+    }
+}
